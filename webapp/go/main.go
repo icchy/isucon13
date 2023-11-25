07fac282449b8015a50b1c76743b39a320910300
nomeaning <nomeaning777@gmail.com>
2023-11-25 13:00:49 +0900

    perf: MySQL接続の設定の更新

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -65,6 +65,7 @@ func connectDB(logger echo.Logger) (*sqlx.DB, error) {
 	conf.Passwd = "isucon"
 	conf.DBName = "isupipe"
 	conf.ParseTime = true
+	conf.InterpolateParams = true
 
 	if v, ok := os.LookupEnv(networkTypeEnvKey); ok {
 		conf.Net = v
@@ -97,7 +98,7 @@ func connectDB(logger echo.Logger) (*sqlx.DB, error) {
 	if err != nil {
 		return nil, err
 	}
-	db.SetMaxOpenConns(10)
+	db.SetMaxOpenConns(60)
 
 	if err := db.Ping(); err != nil {
 		return nil, err
