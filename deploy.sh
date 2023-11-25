b4cfa385aa8e2e80e0e7f0e5de4a3237461cba69
whywaita <whywaita@whywrite.it>
2023-11-25 16:03:26 +0900

    naosita

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,15 @@ HOST=i1
 APPDIR=/home/isucon/webapp
 
 # isupipe
+cd webapp/go
+make build
+cd ../..
 scp webapp/go/isupipe $HOST:/tmp/_isupipe
 ssh $HOST mv /tmp/_isupipe $APPDIR/go/isupipe
+
+# isudns
+scp isudns/isudns $HOST:/tmp/_isudns
+ssh $HOST mv /tmp/_isudns $HOME/isudns
 
 # db
 rsync -av --delete -e ssh webapp/sql/ $HOST:$APPDIR/sql
